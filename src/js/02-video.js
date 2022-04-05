101a8ffede4b591dc56d7de7dfe634b4db532a95
AnthonyUPTN <tagirovantn@gmail.com>
2022-04-05 13:36:08 +0300

    some changes

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+
+const player = new Player(iframe);
+player.setCurrentTime(Number(localStorage.getItem('videoplayer-current-time')));
+
+function playerTime(event) {
+  localStorage.setItem('videoplayer-current-time', event.seconds);
+}
+
+player.on('timeupdate', throttle(playerTime, 1000));
